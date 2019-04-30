dfe717ae237ccea76c31284e935544fd5d5ccb2e
David Freitag <freitagdavid@outlook.com>
2019-04-29 21:42:36 -0400

    pointers/pointers: All done but find_string

--- pointers/pointers.c
+++ pointers/pointers.c
@@ -4,7 +4,7 @@
     Given a character pointer x (that points to an array of chars), and a
     character pointer y, copies the character contents of y over to x. Pointer
     arithmetic is necessary here. Also, make sure x points to a null terminator
-    at its end to terminate it properly. 
+    at its end to terminate it properly.
 
     Example call:
 
@@ -13,46 +13,84 @@
     string_copy(buffer, "Hello!");
     printf("%s", buffer); // Prints "Hello!"
 */
-void string_copy(char *x, char *y)
-{
-
+void string_copy(char *x, char *y) {
+  int i = 0;
+  while (*y != '\0') {
+    x[i] = *y;
+    i++;
+    y++;
+  }
+  x[i] = '\0';
 }
 
 /*
-    Searches the input string `str` for the first instance of the 
+    Searches the input string `str` for the first instance of the
     character `c` (an unsigned char). This function returns a pointer
     that points to the first instance of the character `c` in the
     input string `str`.
 
     Do not use the `strchr` function from the standard library.
 */
-char *find_char(char *str, int c)
-{
-
+char *find_char(char *str, int c) {
+  while (*str != '\0') {
+    if (*str == c) {
+      return str;
+    }
+    str++;
+  }
+  return NULL;
 }
 
 /*
     Searches the input string `haystack` for the first instance of
     the string `needle`. This function returns a pointer that points
     to the first instance of the string `needle` in the input
-    string `haystack`. 
+    string `haystack`.
 
     Do not use the `strstr` function from the standard library.
 */
-char *find_string(char *haystack, char *needle)
-{
+char *find_string(char *haystack, char *needle) {
+  if (needle == NULL) {
+    return NULL;
+  }
+  char *match = NULL;
+  while (*haystack) {
+    if (*haystack == *needle) {
+      match = haystack;
+      while (*needle) {
 
+        char *origNeedle = needle;
+
+        if (*haystack != *needle) {
+          match = NULL;
+          needle = origNeedle;
+          break;
+        }
+
+        haystack++;
+        needle++;
+
+        if (*needle == '\0') {
+          return match;
+        }
+      }
+    }
+
+    haystack++;
+
+    if (*haystack == '\0') {
+      break;
+    }
+  }
 }
 
 #ifndef TESTING
-int main(void)
-{
-    char *found_char = find_char(hello, 'e');
-    char *found_string = find_string(world, "or");
+int main(void) {
+  char *found_char = find_char("hello", 'e');
+  char *found_string = find_string("LambdaSchool", "lambda");
 
-    printf("Found char: %s\n", found_char);
-    printf("Found string: %s\n", found_string);
-
-    return 0;
+  printf("Found char: %s\n", found_char);
+  printf("Found string: %s\n", found_string);
+  return 0;
 }
 #endif
