09673fbe59af354dd23c4d55cd545b23aedec16b
David Freitag <freitagdavid@outlook.com>
2019-04-30 17:44:15 -0400

    malloc/malloc: string_dup, mem_copy working

--- malloc/malloc.c
+++ malloc/malloc.c
@@ -1,18 +1,24 @@
+#include "lib.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "lib.h"
 
 /*
-    Duplicates the input string by dynamically allocating memory for 
+    Duplicates the input string by dynamically allocating memory for
     the duplicate string using `malloc` and then copying the string
     into the allocated memory. Returns a pointer to the allocated memory.
     You may want to use the string_length function to figure out the
     length of the input string.
 */
-char *string_dup(char *src)
-{
 
+char *string_dup(char *src) {
+  int src_length = string_length(src);
+  char *dup_string;
+  dup_string = (char *)malloc(src_length * sizeof(char));
+  for (int i = 0; i < src_length; i++) {
+    dup_string[i] = src[i];
+  }
+  return dup_string;
 }
 
 /*
@@ -20,68 +26,76 @@ char *string_dup(char *src)
     of any type and copies its contents to the destination pointer (dest).
     You may want to cast the input pointers to char pointers first before
     performing the copying. `n` is the amount of data that should be copied
-    from `src` to `dest`. 
+    from `src` to `dest`.
 */
-void mem_copy(void *dest, const void *src, int n)
-{
-
+void mem_copy(void *dest, const void *src, int n) {
+  for (int i = 0; i < n; i++) {
+    *((char *)dest) = *(char *)src;
+    dest++;
+    src++;
+  }
 }
 
 /*
-    Given a pointer of `malloc`'d memory, this function will 
+    Given a pointer of `malloc`'d memory, this function will
     attempt to resize the allocated memory to the new specified
-    size. Any data that was previously in the old `malloc`'d 
-    memory should be intact in the new resized block of memory. 
+    size. Any data that was previously in the old `malloc`'d
+    memory should be intact in the new resized block of memory.
     Some edge cases to consider: how should resizing be handled
-    in the case when old_size < new_size? What about when 
+    in the case when old_size < new_size? What about when
     old_size > new_size?
-    
+
     Do not use the `realloc` function from the standard libary.
 */
-void *resize_memory(void *ptr, int old_size, int new_size)
-{
-
+void *resize_memory(void *ptr, int old_size, int new_size) {
+  int *new_alloc = malloc(new_size);
+  if (old_size < new_size) {
+    mem_copy(new_alloc, ptr, old_size);
+  } else {
+    mem_copy(new_alloc, ptr, new_size);
+  }
+  ptr = new_alloc;
 }
 
 #ifndef TESTING
-int main(void)
-{
-    char *s = "Some string to duplicate.";
-    char *dup = string_dup(s);
+int main(void) {
+  char *s = "Some string to duplicate.";
+  char *dup = string_dup(s);
 
-    printf("Duplicated string: %s\n", dup);
+  printf("Duplicated string: %s\n", dup);
 
-    int numbers[] = {100, 55, 4, 98, 10, 18, 90, 95, 43, 11, 47, 67, 89, 42, 49, 79};
-    int n = sizeof(numbers) / sizeof(numbers[0]);
-    int *target = malloc(n * sizeof(int));
-    
-    mem_copy(target, numbers, n * sizeof(int));
+  int numbers[] = {100, 55, 4,  98, 10, 18, 90, 95,
+                   43,  11, 47, 67, 89, 42, 49, 79};
+  int n = sizeof(numbers) / sizeof(numbers[0]);
+  int *target = malloc(n * sizeof(int));
 
-    printf("Copied array: ");
+  mem_copy(target, numbers, n * sizeof(int));
 
-    for (int i = 0; i < n; i++) {
-        printf("%d ", target[i]);
-    }
+  printf("Copied array: ");
 
-    printf("\n");
+  for (int i = 0; i < n; i++) {
+    printf("%d ", target[i]);
+  }
 
-    char *url = string_dup("http://lambdaschool.com");
-    char *path = string_dup("/students/");
-    int url_length = string_length(url);
-    int path_length = string_length(path);
-    
-    int new_length = url_length - 1 + path_length;
-    char *new_url = resize_memory(url, url_length, new_length);
-    char *p = new_url + url_length;
+  printf("\n");
 
-    while (*path != '\0') {
-        *p = *path;
-        p++;
-        path++;
-    }
+  char *url = string_dup("http://lambdaschool.com");
+  char *path = string_dup("/students/");
+  int url_length = string_length(url);
+  int path_length = string_length(path);
 
-    printf("Full path string: %s\n", new_url);
+  int new_length = url_length - 1 + path_length;
+  char *new_url = resize_memory(url, url_length, new_length);
+  char *p = new_url + url_length;
 
-    return 0;
+  while (*path != '\0') {
+    *p = *path;
+    p++;
+    path++;
+  }
+
+  printf("Full path string: %s\n", new_url);
+
+  return 0;
 }
 #endif
