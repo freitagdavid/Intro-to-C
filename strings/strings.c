17385151f09cb6f4cab38d729b2ca22a3b4be0bf
David Freitag <freitagdavid@outlook.com>
2019-04-30 20:00:56 -0400

    strings/strings: Formatting

--- strings/strings.c
+++ strings/strings.c
@@ -6,11 +6,9 @@
     characters in the string.
     Do not just use the `strlen` function from the standard libary.
 */
-int string_length(char *s)
-{
+int string_length(char *s) {
   int currentChar = 0;
-  while (s[currentChar] != '\0')
-  {
+  while (s[currentChar] != '\0') {
     currentChar++;
   };
   return currentChar;
@@ -22,11 +20,9 @@ int string_length(char *s)
     enough space for the reversed string. Don't forget to terminate
     the reversed string with a null character. Return the rv array.
 */
-char *reverse_string(char *rv, char *s)
-{
+char *reverse_string(char *rv, char *s) {
   int inputLength = string_length(s);
-  for (int i = 0; i < inputLength; i++)
-  {
+  for (int i = 0; i < inputLength; i++) {
     rv[inputLength - i - 1] = s[i];
   }
   rv[inputLength] = '\0';
@@ -34,8 +30,7 @@ char *reverse_string(char *rv, char *s)
 }
 
 #ifndef TESTING
-int main(void)
-{
+int main(void) {
   char quote1[] = "Don't forget to be awesome";
   char quote2[] = "a man a plan a canal panama";
 
